faaca3acfdedbc9db4bdbe4507707c62cc2fc686
MahmoudBakri225 <mahmoudbakri225@gmail.com>
2026-09-07 00:37:27 -0700

    update

--- src/main/java/com/mycompany/shoppingsystem/Customer.java
+++ src/main/java/com/mycompany/shoppingsystem/Customer.java
@@ -1,15 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package com.mycompany.shoppingsystem;
 
 import java.util.ArrayList;
 
-/**
- *
- * @author KA
- */
 public class Customer {
 
     private String name;
--- src/main/java/com/mycompany/shoppingsystem/ShoppingSystem.java
+++ src/main/java/com/mycompany/shoppingsystem/ShoppingSystem.java
@@ -1,12 +1,6 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 package com.mycompany.shoppingsystem;
 
-/**
- *
- * @author KA
- */
+
 public class ShoppingSystem {
 
     public static void main(String[] args) {
